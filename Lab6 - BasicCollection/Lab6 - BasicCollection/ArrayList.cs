b1f35ae991e2ae217536ca1249ce92497088df3e
Reece Wisdom <62401253+ReeceWisdom@users.noreply.github.com>
2021-03-20 13:25:42 +0000

    Add files via upload

---
+++ Lab6 - BasicCollection/Lab6 - BasicCollection/ArrayList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6___BasicCollection
+{
+    class ArrayList
+    {
+        string s1 = "First";
+        string s2 = "Second";
+        string s3 = "Third";
+        string s4 = "Fourth";
+    }
+}
