52463e81271e3dc2d88b361d1fbfb330382abe4a
Reece Wisdom <62401253+ReeceWisdom@users.noreply.github.com>
2021-03-20 13:23:38 +0000

    Add files via upload

---
+++ Lab3 - Queue/Lab3 - Queue/IntQueue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3___Queue
+{
+    class IntQueue
+    {
+        private readonly int maxsize = 10;
+        private int[] store;
+        private int head = 0;
+        private int tail = 0;
+        private int numItems;
+
+        public IntQueue()
+        {
+            store = new int[maxsize];
+        }
+        public IntQueue(int size)
+        {
+            maxsize = size;
+            store = new int[maxsize];
+        }
+        public void Enqueue(int value)
+        {
+            numItems++;
+            store[tail] = value;
+            if (++tail == maxsize)
+                tail = 0;
+        }
+        public int Dequeue()
+        {
+            int headItem;
+            numItems--;
+            headItem = store[head];
+
+            if (++head == maxsize)
+            {
+                head = 0;
+            }
+            return headItem;
+        }
+        public int Peek()
+        {
+            return store[tail--];
+        }
+        public bool IsEmpty()
+        {
+            return head == 0;
+        }
+        public bool IsFull()
+        {
+            return tail == maxsize;
+        }
+    }
+}
