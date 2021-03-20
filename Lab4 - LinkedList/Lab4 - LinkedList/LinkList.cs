6c2c91d1457b556dcdd5f4a798c5e3714c7b518e
Reece Wisdom <62401253+ReeceWisdom@users.noreply.github.com>
2021-03-20 13:24:24 +0000

    Add files via upload

---
+++ Lab4 - LinkedList/Lab4 - LinkedList/LinkList.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab4___LinkedList
+{
+    class LinkList
+    {
+        private Link list = null;
+        public void AddItem(int item)
+        {
+            list = new Link(item, list);           
+        }
+
+        public string DisplayItems()
+        {
+            Link temp = list;
+            string buffer = "";
+            while (temp != null)
+            {
+                Console.WriteLine(temp.Data); //displays item
+                temp = temp.Next; //next item
+            }
+            return buffer;
+        }
+
+        public int NumberOfItems()
+        {
+            Link temp = list;
+            int count = 0;
+            while (temp != null)
+            {
+                temp = temp.Next;
+                count++;
+            }
+            return count;
+        }
+        
+        public bool IsPresentItem(int item)
+        {
+            Link temp = list;
+            while (temp != null)
+            {
+                if (temp.Data == item)
+                {
+                    Console.WriteLine("Searched Item = {0}", temp.Data);
+                    return true;
+                }
+                else
+                    temp = temp.Next;
+            }
+            Console.WriteLine("Searched Item Cannot be Found...");
+            return false;
+        }
+
+        public void RemoveItem(int item)
+        {
+            Link temp = list;
+            LinkList tempList = new LinkList();
+            while (temp != null)
+            {
+                if (temp.Data != item)
+                {
+                    tempList.AddItem(temp.Data);
+                }
+                temp = temp.Next;
+            }
+            list = tempList.list;
+        }
+
+        public void InsertInOrder(int item)
+        {
+            bool added = false;
+            Link temp = list;
+            LinkList tempList = new LinkList();
+            while(temp != null)
+            {
+                if ((temp.Data >= item) && (added == false))
+                {
+                    tempList.AddItem(item);
+                    added = true;
+                }
+                tempList.AddItem(temp.Data);
+                temp = temp.Next;
+            }
+            list = tempList.list;
+        }
+    }
+}
---
+++ Lab4 - LinkedList/Lab4 - LinkedList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab4___LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkList testList = new LinkList();
+            Random randgen = new Random();
+
+            for (int i = 1; i < 8; i++)
+                testList.AddItem(i);
+                //testList.AddItem(randgen.Next(20));
+
+            testList.DisplayItems();
+
+            Console.WriteLine("\nNo. of Items = {0}\n", testList.NumberOfItems());
+            Console.WriteLine("Is 17 Present? --- {0}\n", testList.IsPresentItem(17));
+            Console.WriteLine("Is 3 Present? --- {0}\n", testList.IsPresentItem(3));
+
+            testList.RemoveItem(3);
+            testList.InsertInOrder(12);
+            testList.DisplayItems();
+
+            Console.WriteLine("\nNo. of Items = {0}\n", testList.NumberOfItems());
+
+            Console.ReadKey();
+        }
+    }
+}
